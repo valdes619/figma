ded85d460d58caa913ef6ab597408c2976d592ad
valdes619 <valdesngami4@gmail.com>
2025-03-21 20:42:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+<script>
+alert('Bonjour');
+// la fonction affiche une boîte de dialogue avec "Bonjour"
+</script>
---
+++ tesst.js
@@ -0,0 +1,6 @@
+function menutoogle() {
+ nav=document.getElementsByTagName("nav");
+ nav.style.display="flex";
+ 
+}
+   
